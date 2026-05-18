c34b87fe65a5be9ab74718fa09eb39a932fb5e05
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-05-18 15:16:37 -0300

    Exercícios finalizados

--- exercicios/aula014-ex/ex016/script.js
+++ exercicios/aula014-ex/ex016/script.js
@@ -2,14 +2,43 @@ const botao = document.getElementById("btn-contar")
 botao.addEventListener("click", contar)
 
 function contar(){
-    const inicio = document.getElementById("in1").value
-    const final = document.getElementById("in2").value
-    const passo = document.getElementById("ipasso").value
+    const inicioRaw = document.getElementById("in1").value
+    const finalRaw = document.getElementById("in2").value
+    let passoRaw = document.getElementById("ipasso").value
     const rstd = document.getElementById("res")
 
-    if (inicio.length == 0 || final.length == 0) {
+    if (inicioRaw.length == 0 || finalRaw.length == 0) {
         rstd.innerText = "Impossível contar."
-    } else if (Number(inicio) == Number(final)) {
-        rstd.innerText = "Coloque valores diferentes."
+        return
     }
+
+    const inicio = Number(inicioRaw)
+    const final = Number(finalRaw)
+    let passo = Number(passoRaw)
+
+    if (inicio == final) {
+        rstd.innerText = "Coloque valores diferentes."
+        return
+    } 
+    
+    if (passo == 0 || passoRaw.length == 0 ) {
+        alert("Passo inválido! Considerando PASSO 1")
+        passo = 1
+    }
+
+    let contagem = ""
+
+    passo = Math.abs(passo)
+
+    if (inicio < final) {
+        for (let c = inicio; c <= final; c += passo) {
+            contagem += `${c}  `
+        }
+    } else {
+        for (let c = inicio; c >= final; c -= passo) {
+            contagem += `${c}  `
+        }
+    }
+
+    rstd.innerText = contagem
 }
--- exercicios/aula014-ex/ex017/script.js
+++ exercicios/aula014-ex/ex017/script.js
@@ -1,3 +1,22 @@
-var agora = new Date()
-var hora = agora.getHours()
+const botao = document.getElementById("btn-gerar") //BOTÃO GERAR
+botao.addEventListener("click", gerar)
 
+function gerar(){
+    const numRaw = document.getElementById("inum").value //INPUT 
+    
+    if (numRaw.length == 0) {
+        alert("Digite um número!")
+        return
+    } 
+
+    const num = Number(numRaw) //NÚMERO DE REFERÊNCIA
+    const rstd = document.getElementById("res") //RESULTADO
+    let tabuada = "" //TABUADA
+
+    for (let c = 1; c <= 10; c++) {
+        const conta = num * c //MULTIPLICAÇÃO
+        tabuada += `<p>${num} * ${c} = ${conta}</p>`
+    }
+
+    rstd.innerHTML = tabuada 
+}
